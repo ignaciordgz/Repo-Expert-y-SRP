cba4bd9413c53a177e3e168beb0662f27f9ab7f0
Ignacio Rodriguez <IGNACIOE.RODRIGUEZ@CORREO.UCU.EDU.UY>
2023-09-04 18:52:51 -0300

    Commit Ejercicio 1

---
+++ Ejercicio 1/src/Library/Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+// No se cumple el principio de SRP ya que la clase Book tiene mas de una razon para cambiar
+
+
+namespace Ucu.Poo.Expert
+{
+    public class Book
+    {
+
+        private string Title { get ; }
+        private string Author { get ; }
+        private string Code { get ;  }
+
+        public Book(String title, String author, String code)
+        {
+            this.Title = title;
+            this.Author = author;
+            this.Code = code;
+        }
+    }
+    
+    public class BookUbication
+    {
+        private string LibrarySector { get ; set; }
+        private string LibraryShelve { get ; set; }
+
+        public void ShelveBook(Book book,string sector,string shelve)
+        {
+            this.LibrarySector = sector;
+            this.LibraryShelve = shelve;
+        }
+    }
+}
---
+++ Ejercicio 1/src/Program/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ucu.Poo.Expert
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            Book book1 = new Book("Design Patterns","Erich Gamma & Others","001-034");
+            Book book2 = new Book("Pro C#","Troelsen","001-035");
+            
+            BookUbication bookUbication1 = new BookUbication();
+            bookUbication1.ShelveBook(book1,"B","3");
+            
+            BookUbication bookUbication2 = new BookUbication();
+            bookUbication2.ShelveBook(book2,"A","7");
+        }
+    }
+}
